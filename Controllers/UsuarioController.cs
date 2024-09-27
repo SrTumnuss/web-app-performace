ac50b414e675c151c61296bd7ee6fabfce2abd85
Lucas Barros da Silva <lucas.silvaa1307@gmail.com>
2024-09-26 21:37:36 -0300

    Implementação do cach

--- Controllers/UsuarioController.cs
+++ Controllers/UsuarioController.cs
@@ -1,6 +1,8 @@
 ﻿using Dapper;
 using Microsoft.AspNetCore.Mvc;
 using MySqlConnector;
+using Newtonsoft.Json;
+using StackExchange.Redis;
 using web_app_performace.Model;
 
 namespace web_app_performace.Controllers
@@ -9,14 +11,34 @@ namespace web_app_performace.Controllers
     [ApiController]
     public class UsuarioController : ControllerBase
     {
+        private static ConnectionMultiplexer redis;
+        
         [HttpGet]
         public async Task<IActionResult> GetUsuario()
         {
+
+            string key = "getUsuario";
+            redis = ConnectionMultiplexer.Connect("localhost:6379");
+            IDatabase db = redis.GetDatabase();
+            await db.KeyExpireAsync(key, TimeSpan.FromSeconds(10));
+            string user = await db.StringGetAsync(key);
+
+            if (!string.IsNullOrEmpty(user)) {
+
+                Usuario u = JsonConvert.DeserializeObject<Usuario>(user);
+
+
+                return Ok(u);  
+            }
+
             string connectionString = "Server=localhost;Database=sys;User=root;Password=123;";
             using var connection = new MySqlConnection(connectionString);
             await connection.OpenAsync();
             string query = "select Id, Nome, Email from usuarios;";
             var usuarios = await connection.QueryAsync<Usuario>(query);
+
+            string usuariosJson = JsonConvert.SerializeObject(usuarios);
+            await db.StringSetAsync(key, usuariosJson);
 
             return Ok(usuarios);
             }
--- obj/Debug/net8.0/web-app-performace.AssemblyInfo.cs
+++ obj/Debug/net8.0/web-app-performace.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("web-app-performace")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a3ef66d02892f53531e6bb326d4e596096402e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("web-app-performace")]
 [assembly: System.Reflection.AssemblyTitleAttribute("web-app-performace")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
