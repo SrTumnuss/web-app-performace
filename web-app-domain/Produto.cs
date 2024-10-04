a0847e74f49f7974588bc008b513272e2c89ea48
Lucas Barros da Silva <lucas.silvaa1307@gmail.com>
2024-10-03 21:43:04 -0300

    Criação do produto e refatoração

---
+++ web-app-domain/Produto.cs
@@ -0,0 +1,11 @@
+﻿namespace web_app_domain
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+        public int QuantidadeEstoque { get; set; }
+        public DateTime DataCriacao { get; set; }
+    }
+}
---
+++ web-app-performance/Controllers/ProdutoController.cs
@@ -0,0 +1,84 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc;
+using MySqlConnector;
+using Newtonsoft.Json;
+using StackExchange.Redis;
+using web_app_domain;
+using web_app_repository;
+
+namespace web_app_performance.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutoController : ControllerBase
+    {
+        private static ConnectionMultiplexer redis;
+        private readonly IProdutoRepository _repository;
+
+        public ProdutoController(IProdutoRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProduto()
+        {
+            string key = "getproduto";
+            redis = ConnectionMultiplexer.Connect("localhost:6379");
+            IDatabase db = redis.GetDatabase();
+            await db.KeyExpireAsync(key, TimeSpan.FromSeconds(10));
+            string produto = await db.StringGetAsync(key);
+
+            if (!string.IsNullOrEmpty(produto))
+            {
+                return Ok(produto);
+            }
+
+            var produtos = await _repository.ListarProdutos();
+            string produtosJson = JsonConvert.SerializeObject(produtos);
+            await db.StringSetAsync(key, produtosJson);
+            return Ok(produtos);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Produto produto)
+        {
+            await _repository.SalvarProduto(produto);
+
+            // Apagar o cache
+            string key = "getproduto";
+            redis = ConnectionMultiplexer.Connect("localhost:6379");
+            IDatabase db = redis.GetDatabase();
+            await db.KeyDeleteAsync(key);
+
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put([FromBody] Produto produto)
+        {
+            await _repository.AtualizarProduto(produto);
+
+            // Apagar o cache
+            string key = "getproduto";
+            redis = ConnectionMultiplexer.Connect("localhost:6379");
+            IDatabase db = redis.GetDatabase();
+            await db.KeyDeleteAsync(key);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _repository.RemoverProduto(id);
+
+            // Apagar o cache
+            string key = "getproduto";
+            redis = ConnectionMultiplexer.Connect("localhost:6379");
+            IDatabase db = redis.GetDatabase();
+            await db.KeyDeleteAsync(key);
+
+            return Ok();
+        }
+    }
+}
